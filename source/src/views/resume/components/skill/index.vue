bdab2560a59ad73d6345219c72be12e219a4196f
Rhodri-hc <13622450494@163.com>
2022-08-06 17:40:26 +0800

    更新简历

--- source/src/views/resume/components/skill/index.vue
+++ source/src/views/resume/components/skill/index.vue
@@ -9,13 +9,13 @@
 <template>
   <div class="skill">
     <h3 class="skill__title">技能清单</h3>
-    <ul 
-      v-for="(item, index) of skillList" 
-      class="skill__content"
-      :key="index"
-    >
-      <li>{{ item }}</li>
-    </ul>
+      <ul 
+        v-for="(item, index) of skillList" 
+        class="skill__content"
+        :key="index"
+      >
+        <li>{{ item }}</li>
+      </ul>
   </div>
 </template>
 <script>
@@ -26,9 +26,9 @@ export default {
   data() {
     return {
       skillList:[
-        "对 Vue2、Vue3 部分核心原理有深入学习经验及个人见解；",
-        "具有公司自研组件库开发经验，对 Vue 主流 UI 框架（Element-UI）有深入学习经验；",
-        "熟悉掌握 Node.js，对服务端框架 egg.js 编写 BFF 层有开发经验",
+        "熟练使用 Vue.js 前端开发框架，并对 Vue2、Vue3 部分核心原理有深入学习经验及个人见解；",
+        "熟练使用 Vue 主流 UI 框架（Element-UI），并有深入学习经验，具有公司自研组件库开发经验；",
+        "熟悉使用 Node.js，对服务端框架 egg.js 编写 BFF 层有开发经验",
         "熟悉 Electron 构建桌面应用程序；",
         "熟悉小程序开发框架 ，具有小程序 mpvue 框架开发经验；",
         "熟悉 Tailwind CSS 框架，以及 Scss，Stylus 等 CSS 预处理器；",
@@ -46,6 +46,7 @@ export default {
 </script>
 <style scoped lang="scss">
 .skill{
+  padding-bottom: 15px;
   border-bottom: 1px solid #000;
   &__title{
     display: flex;
@@ -63,7 +64,7 @@ export default {
   }
 
   &__content{
-    
+    margin: 10px 0;
   }
 }
 
