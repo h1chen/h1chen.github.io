f76c8de243b2c5fbaa95f9ab6a2c9a1302dab72f
Rhodri-hc <13622450494@163.com>
2022-10-13 17:22:10 +0800

    更新简历技能和项目经历

--- source/src/views/resume/components/skill/index.vue
+++ source/src/views/resume/components/skill/index.vue
@@ -26,15 +26,13 @@ export default {
   data() {
     return {
       skillList:[
-        "熟练使用 Vue.js 前端开发框架，并对 Vue2、Vue3 部分核心原理有深入学习经验及个人见解",
+        "熟练使用 Vue.js 前端开发框架，了解 Vue2、Vue3 核心原理，了解 Nuxt.js 搭建服务端渲染",
         "熟练使用 Vue 主流 UI 框架（Element-UI），并有深入学习经验，具有公司自研组件库开发经验",
-        "熟悉 Electron 构建桌面应用程序",
-        "熟悉小程序开发框架 ，具有小程序 mpvue 框架开发经验",
-        "熟悉 Tailwind CSS 框架，以及 Scss，Stylus 等 CSS 预处理器",
-        "熟悉前端性能优化方法，熟悉 http 协议，熟悉各种 Web 标准规范",
+        "熟悉 Electron 构建桌面应用程序，熟悉小程序开发框架 ，具有小程序 mpvue 框架开发经验",
+        "熟悉前端性能优化方法，熟悉 http、TCP 协议，熟悉各种 Web 标准规范，了解设计模式并运用在开发中",
+        "熟悉前端工程化，熟悉使用 webpack 构建工具并了解原理，开发 plugin 解决实际问题",
         "熟悉 ESLint 等代码检查工具的配置及使用，有良好的代码习惯",
         "了解使用 Node.js，对服务端框架 egg.js 编写 BFF 层有开发经验",
-        "了解 webpack 构建工具",
       ]
     };
   },
